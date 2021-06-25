2d0e0680d7e995ba433f5cdd4d63fe1e7972b17f
Victor Efedi <victorefedi@Victors-MacBook-Pro.local>
2021-06-25 09:20:34 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+locals {
+  ps_tags = {
+      PS-Managed = "Do-Not-Modify-Delete-Created-By-PhantomStack"
+    }
+}
+
+module "tf_azurerm_vnet" {
+  source = "github.com/StackGuardian/terraform-azurerm-vnet?ref=v2.0.0"
+
+  //for resource group
+  resource_group_name = var.resource_group_name
+  
+  // For Vnet
+  vnet_name     = var.vnet_name
+  address_space = var.address_space
+  dns_servers   = var.dns_servers
+
+  // for tags
+  tags          = merge(local.ps_tags,var.tags)
+
+  // for subnet
+  subnet_names                                            = var.subnet_names
+  subnet_prefixes                                         = var.subnet_prefixes
+  subnet_service_endpoints                                = var.subnet_service_endpoints
+  subnet_enforce_private_link_endpoint_network_policies   = var.subnet_enforce_private_link_endpoint_network_policies
+  subnet_enforce_private_link_service_network_policies    = var.subnet_enforce_private_link_service_network_policies
+}
